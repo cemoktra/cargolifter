33baab9ee111cc808c414618a339cefe0aa4af79
cemoktra <b.schubert82@gmail.com>
2021-10-03 07:19:13 +0200

    fix auth

--- src/auth/null.rs
+++ src/auth/null.rs
@@ -2,7 +2,7 @@ use super::Authenticator;
 
 pub struct NullAuthenticator;
 
-pub impl Authenticator for NullAuthenticator {
+impl Authenticator for NullAuthenticator {
     fn authenticate(&self, token: &str) -> bool {
         log::info!("Authenticating token '{}'", token);
         true
